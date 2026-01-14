7b132d69482099557ea80eb49f26aacb846c6a41
gamoLG <224871@unsaac.edu.pe>
2026-01-14 11:24:56 -0500

    agragar ejercicio matriz 3x3 y que muestre su diagonal

---
+++ matriz3x3.cpp
@@ -0,0 +1,50 @@
+//una matriz 3x3 y que muestre su diagonal princial
+#include<iostream>
+using namespace std;
+int main()
+{
+	int matriz[3][3];
+	
+	// guardamos elementos en la matriz
+	for(int i=0;i<3;i++)
+	{
+		for(int j=0;j<3;j++)
+		{
+			cout<<"ingrese el elemento: ["<<i<<"]["<<j<<"]: ";
+			cin>>matriz[i][j];
+		}
+		cout<<endl;
+	}
+	// mostrar la matriz general
+	for(int i=0;i<3;i++)
+	{
+		for(int j=0;j<3;j++)
+		{
+			cout<<matriz[i][j];
+		}
+		cout<<endl;
+	}
+	
+	//mostrar la diagonal principal 1ra forma
+	/*
+	cout<<endl;
+	for(int i=0;i<3;i++)
+	{
+		for(int j=i;j<3;j++)
+		{
+		   cout<<matriz[i][j];
+		   break;	
+		}
+		cout<<endl;
+	}
+	
+	*/
+	
+	// 2da forma
+	
+	for(int i=0;i<3;i++)
+	{
+		cout<<matriz[i][i]<<endl;
+	}
+	return 0;
+}
