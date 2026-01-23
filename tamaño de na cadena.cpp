1731a06a06ef7aecbbc4ac1e2acce578a46e510a
gamoLG <224871@unsaac.edu.pe>
2026-01-23 09:19:32 -0500

    tamañano de las cadenas

---
+++ tamaño de na cadena.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string.h>  //-> para el strlen()
+#include<string>
+using namespace std;
+int main()
+{
+	char palabra[]="hsodifhsñfejf";
+	int longitud=0;
+	longitud=strlen(palabra); // devulve ell tamaño de la palabra en enteros
+	cout<<"la longitud es: "<<longitud<<endl;
+	//ahora con la biblioteca string
+	string palabra2="fnvdlfvndklvnvkf";
+	int longitud2=0;
+	longitud2=palabra2.length();
+	
+	cout<<"la longitud 2 es: "<<longitud2<<endl;
+	
+	//ahora con zise
+	 string palabra3="sfkbsrkvbsbvvbc";
+	 int longitud3=palabra3.size();
+	 cout<<"longitud de la palabra 3: "<<longitud3<<endl;
+	
+	return 0;
+}
