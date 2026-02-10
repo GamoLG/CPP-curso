0caa1fe530faafed59625cb6d3d433be0a2eb142
gamoLG <224871@unsaac.edu.pe>
2026-02-10 10:24:22 -0500

    ordenamiento por insercion

--- ordenamiento por insercion.cpp
+++ ordenamiento por insercion.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 int main()
 {
-	//vector
+	/*
+	//vector	
 	vector<int> numeros={4,15,4,1,10,9,11};
 	//mostrar vector original
 	for(int i=0;i<numeros.size();i++)
@@ -31,5 +32,30 @@ int main()
 		cout<<num<<" ";
 	}
 	
+	
+	*/
+	vector<int> numeros4={1,4,2,5,10,9};
+	for(int num: numeros4)
+	{
+		cout<<num<<" ";
+	}
+	int pos, aux;
+	for(int i=0;i<numeros4.size();i++)
+	{
+		pos=i;
+		aux=numeros4[i];
+		while(pos>0 && numeros4[pos-1]>aux)
+		{
+			numeros4[pos]=numeros4[pos-1];
+			pos--;
+		}
+		numeros4[pos]=aux;
+	}
+	//mostrar el vector ordenado
+	cout<<endl;
+	for(int num: numeros4)
+	{
+		cout<<num<<" ";
+	}
 	return 0;
 }
